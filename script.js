fcad807b8a3f47ecd4d03678d0ad21c5f4e13b7d
Sophie Spang <soph8111@stud.kea.dk>
2021-08-30 20:27:03 +0200

    value === 0

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const input = document.querySelector("#inputID");
 const dropdown = document.querySelector("#dropdown");
 const output = document.querySelector("#outpu");
 const button = document.querySelector("#button");
+let newInput;
 
 window.addEventListener("DOMContentLoaded", start);
 
@@ -25,33 +26,27 @@ function readInput() {
 }
 
 function readDropdown() {
-  console.log("readDropdown");
+  console.log("Drop down value: " + dropdown.value);
   if (dropdown.value === "0") {
-      // If input is a first name: Make the first character in a name uppercase, and the rest lowercase
-
+    // If input is a first name: Make the first character in a name uppercase, and the rest lowercase
+    newInput = input.value.substring(0, 1).toUpperCase() + input.value.substring(2).toLowerCase();
+    console.log(newInput);
   } else if (dropdown.value === "1") {
-      //If input is a full name: Find the first name
-  
+    //If input is a full name: Find the first name
   } else if (dropdown.value === "2") {
-      //If input is a full name: Find the length of the first name
-
+    //If input is a full name: Find the length of the first name
   } else if (dropdown.value === "3") {
-      // If input is a full name: Find the middle name start and end position, and the middle name itself in a full name string
-
+    // If input is a full name: Find the middle name start and end position, and the middle name itself in a full name string
   } else if (dropdown.value === "4") {
-      // If input is a filename: Check if a filename is .png or .jpg
-
+    // If input is a filename: Check if a filename is .png or .jpg
   } else if (dropdown.value === "5") {
-      // If input is a password: Hide a password with the correct number of *s
-
+    // If input is a password: Hide a password with the correct number of *s
   } else if (dropdown.value === "6") {
-      // With any input: Make the third character uppercase
-
-  } else (dropdown.value === "7") {
-      // With any input: Make a character uppercase, if it follows a space or a hyphen
-
+    // With any input: Make the third character uppercase
+  } else {
+    // With any input: Make a character uppercase, if it follows a space or a hyphen
+    console.log("hej");
   }
-}
 
   writeResult();
 }
